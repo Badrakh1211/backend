24b98c792037a7e69c8bcfa35f406b04633f28a3
Badrakh1211 <badrakh1211@gmail.com>
2024-03-25 18:43:07 +0800

    first commit

---
+++ src/graphql/schemas/post-schema.ts
@@ -0,0 +1,52 @@
+import gql from "graphql-tag";
+
+export const postTypeDefs = gql`
+  type Comment {
+    id: ID!
+    postId: ID!
+    username: String!
+    imageUrl: String!
+    text: String!
+  }
+  type Post {
+    id: ID!
+    userId: ID!
+    username: String!
+    userImage: String!
+    description: String!
+    latitude: String
+    longitude: String
+    imageUrl: String!
+    comments: [Comment]
+  }
+
+  input PostCreateInput {
+    userId: String!
+    description: String!
+    latitude: String
+    longitude: String
+    imageUrl: String!
+    username: String!
+    userImage: String!
+  }
+
+  input CommentCreateInput {
+    postId: ID!
+    text: String!
+    username: String!
+    imageUrl: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+    getPost(id: ID!): Post
+  }
+
+  type Mutation {
+    createPost(input: PostCreateInput!): Post!
+    deletePost(id: ID!): Post
+    updatePost(id: ID!, input: PostCreateInput!): Post
+    createComment(input: CommentCreateInput!): Comment
+    deleteComment(id: ID!): Comment
+  }
+`;
